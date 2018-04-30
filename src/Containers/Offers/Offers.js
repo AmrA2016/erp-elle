85a717f6fd3455dc6caa76d9fbe3f0531ea37e30
karimehabahmed <karimehabahmed@gmail.com>
2018-04-30 19:04:57 +0200

    offers container

--- src/Containers/Offers/Offers.js
+++ src/Containers/Offers/Offers.js
@@ -5,9 +5,9 @@ import './offers.css';
 
 class Offers extends Component {
     state = {
-        id: 'Offers',
+        id: 'offers',
         image: 'https://i.imgur.com/USjUi0h.jpg',
-        page_title: 'Terms & Conditions',
+        page_title: ' Get 10% Off on these new services',
         side_title: 'Offers',
         content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui natus quas eaque culpa quod ipsa' +
         ' dolorum minima dignissimos quasi deleniti. Reprehenderit ab, commodi vel iste, modi a. Odio, dolores, qui?' +
@@ -22,7 +22,7 @@ class Offers extends Component {
     };
     render() {
         return (
-            <div id={this.state.id} className="Offers container-fluid">
+            <div id={this.state.id} className="offers container-fluid">
                 <div className="row">
                     <SideImage image={this.state.image}/>
                     <SideContent page_title={this.state.page_title}
